da6f00c13b93faee60b1feeebe46d340a1fb29f5
ozan isgor <ozanisgor11@gmail.com>
2023-01-10 14:42:15 +0300

    remove: unnecessary starting code&files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="text-5xl font-bold">Hello world!</h1>
-        <button className="btn">Click</button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world!</h1>
     </div>
   );
 }
