64f9b4780f133764354a15e86a474a2ec4e434c7
ozan isgor <ozanisgor11@gmail.com>
2023-01-12 16:18:04 +0300

    add: UserSearch component

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useContext } from 'react';
+import { useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import UserItem from './UserItem';
 import GithubContext from '../../context/github/GithubContext';
